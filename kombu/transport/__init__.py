65b98103d996ab0060dc3b153253f11539f6fd0c
Alex Gaynor <alex.gaynor@gmail.com>
2013-12-18 15:26:22 -0800

    Initial transport for the OpenStack Marconi queueing project

--- kombu/transport/__init__.py
+++ kombu/transport/__init__.py
@@ -69,6 +69,7 @@ TRANSPORT_ALIASES = {
     'zmq': 'kombu.transport.zmq:Transport',
     'amqplib': 'kombu.transport.amqplib:Transport',
     'qpid': 'kombu.transport.qpid:Transport',
+    'marconi': 'kombu.transport.marconi:Transport',
 }
 
 _transport_cache = {}
